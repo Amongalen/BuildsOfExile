9123ca481d57a6def77d474862cfb9c4fd344095
Adam Parys <aparys@ekokal.com>
2021-08-30 20:47:53 +0200

    Fixed thrown exception when setting timezone for anonymous user

--- GuideToExile/middleware.py
+++ GuideToExile/middleware.py
@@ -10,7 +10,7 @@ class TimezoneMiddleware:
     def __call__(self, request):
         # storing timezone both in DB and session, taking from session first
         tzname = request.session.get('django_timezone')
-        if not tzname and request.user:
+        if not tzname and not request.user.is_anonymous:
             tzname = request.user.user_profile.timezone
         if tzname and tzname in pytz.common_timezones:
             timezone.activate(pytz.timezone(tzname))
